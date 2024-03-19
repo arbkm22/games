f9ff638d8bf6974fe6f939974bbc72cecfa019fd
Bhaskar Mahto <arbkm22@gmail.com>
2024-03-19 23:59:00 +0530

    tictactoe

---
+++ terminal/tic-tac-toe/TicTacToe.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Function to print Tic Tac Toe
+void printBoard(char board[3][3]) {
+    std::cout << "  1 2 3\n";
+    for (int i = 0; i < 3; i++) {
+        std::cout << i + 1 << " ";
+        for (int j = 0; j < 3; j++) {
+            std::cout << board[i][j];
+            if (j < 2) {
+                std::cout << "|";
+            }
+        }
+        std::cout << "\n";
+        if (i < 2) {
+            std::cout << "  -+-+-\n";
+        }
+    }
+}
+
+bool isGameOn = true;
+
+int main() {
+    // Creating an empty Tic Tac Toe board
+    char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    std::cout << "bool value: " << isGameOn << std::endl;
+    // Printing the Tic Tac Toe board
+    while (isGameOn) {
+        printBoard(board);
+        int row, col;
+        cout << "Enter the coordinates(row, col): ";
+        cin >> row >> col;
+        if (row > 3 || col > 3 || row < 1 || col < 1) {
+            cout << "Invalid input, please enter the correct coordinates" << endl;
+            continue;
+        }
+        std::cout << "do you want to continue the game: " << std::endl;
+        string input;
+        cin >> input;
+        if (input == "no") {
+            isGameOn = false;
+            break;
+        }
+    }
+    cout << "Thanks for playing the game!" << endl;
+
+    return 0;
+}
