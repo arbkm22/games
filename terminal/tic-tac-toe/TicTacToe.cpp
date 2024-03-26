3112c7b0f11d2a3489e42e6b651a9feb79ffed2f
Bhaskar Mahto <arbkm22@gmail.com>
2024-03-26 19:43:13 +0530

    solved some, broke some

--- terminal/tic-tac-toe/TicTacToe.cpp
+++ terminal/tic-tac-toe/TicTacToe.cpp
@@ -45,10 +45,6 @@ bool gameWon(char board[3][3], int row, int col, string whichPlayer) {
     else if (whichPlayer == "second") {
         toMatch = 'O';
     }
-
-    if (board[row][0] == board[row][1]) {
-        cout << "matching: " << toMatch << endl;
-    }
     
     if (board[row][0] == toMatch && board[row][1] == toMatch && board[row][2] == toMatch) {
         return true;
@@ -96,6 +92,19 @@ void resetBoard(char board[3][3]) {
     }
 }
 
+bool boardFilled(char board[3][3]) {
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<3; j++) {
+            if (board[i][j] != ' ') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// TODO: fix when user wants to play again
+// TODO: check if board is filled
 int main() {
     // Creating an empty Tic Tac Toe board
     char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
@@ -116,23 +125,37 @@ int main() {
             }
         }
         bool winner = gameWon(board, row, col, whichPlayer);
-        if (winner)
+        if (winner) {
+            cout << "winner val: " << winner << endl;
             isGameFinished = true;
-         else {
+        }
+        else {
             if (isValidMove) {
                 if (whichPlayer == "first")
                     whichPlayer = "second";
                 else if (whichPlayer == "second") 
                     whichPlayer = "first";  
             }
-         } 
+        } 
+        bool isBoardFilled = boardFilled(board);
+        if (isBoardFilled)
+            isGameFinished = true;
+
         if (isGameFinished) {
             printBoard(board);
+            if (whichPlayer == "first") {
+                cout << "Player 1 has won the game." << endl;
+            }
+            else if (whichPlayer == "second") {
+                cout << "Player 2 has won the game." << endl;
+            }
             cout << "Do you want to play again: " << endl;
             string answer;
             cin >> answer;
             if (answer == "yes") {
                 isGameOn = true;
+                // winner = false;
+                isGameFinished = false;
                 resetBoard(board);
             } 
             else if (answer == "no") {
