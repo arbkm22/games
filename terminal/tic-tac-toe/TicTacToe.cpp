2c78cbc0732a31f274781bdb9693cc2121e73fee
Bhaskar Mahto <arbkm22@gmail.com>
2024-03-26 20:03:33 +0530

    fixed some more

--- terminal/tic-tac-toe/TicTacToe.cpp
+++ terminal/tic-tac-toe/TicTacToe.cpp
@@ -67,17 +67,17 @@ bool gameWon(char board[3][3], int row, int col, string whichPlayer) {
         }
     }
     else if (row == 0 && col == 2) {
-        if (board[row][col] == toMatch && board[row+1][col-1] == toMatch && board[row+2][col-2]) {
+        if (board[row][col] == toMatch && board[row+1][col-1] == toMatch && board[row+2][col-2] == toMatch) {
             return true;
         }
     }
     else if (row == 2 && col == 0) {
-        if (board[row][col] == toMatch && board[row-1][col+1] == toMatch && board[row-2][col+2]) {
+        if (board[row][col] == toMatch && board[row-1][col+1] == toMatch && board[row-2][col+2] == toMatch) {
             return true;
         }
     }
     else if (row == 2 && col == 2) {
-        if (board[row][col] == toMatch && board[row-1][col-1] == toMatch && board[row-2][col-2]) {
+        if (board[row][col] == toMatch && board[row-1][col-1] == toMatch && board[row-2][col-2] == toMatch) {
             return true;
         }
     }
@@ -95,7 +95,7 @@ void resetBoard(char board[3][3]) {
 bool boardFilled(char board[3][3]) {
     for (int i=0; i<3; i++) {
         for (int j=0; j<3; j++) {
-            if (board[i][j] != ' ') {
+            if (board[i][j] == ' ') {
                 return false;
             }
         }
@@ -103,8 +103,6 @@ bool boardFilled(char board[3][3]) {
     return true;
 }
 
-// TODO: fix when user wants to play again
-// TODO: check if board is filled
 int main() {
     // Creating an empty Tic Tac Toe board
     char board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
@@ -136,25 +134,30 @@ int main() {
                 else if (whichPlayer == "second") 
                     whichPlayer = "first";  
             }
-        } 
+        }
         bool isBoardFilled = boardFilled(board);
         if (isBoardFilled)
             isGameFinished = true;
 
         if (isGameFinished) {
             printBoard(board);
-            if (whichPlayer == "first") {
-                cout << "Player 1 has won the game." << endl;
+            if (isBoardFilled) {
+                cout << "The match ended in a draw." << endl;
             }
-            else if (whichPlayer == "second") {
-                cout << "Player 2 has won the game." << endl;
+            else {
+                if (whichPlayer == "first") {
+                    cout << "Player 1 has won the game." << endl;
+                }
+                else if (whichPlayer == "second") {
+                    cout << "Player 2 has won the game." << endl;
+                }
             }
             cout << "Do you want to play again: " << endl;
             string answer;
             cin >> answer;
             if (answer == "yes") {
                 isGameOn = true;
-                // winner = false;
+                winner = false;
                 isGameFinished = false;
                 resetBoard(board);
             } 
