b47b06bc48d62b86c4a3eaf6dfede9076dad8c82
Bhaskar Mahto <arbkm22@gmail.com>
2024-03-31 00:57:01 +0530

    fixed click functionality

--- webapp/tic-tac-toe/src/components/Cell.js
+++ webapp/tic-tac-toe/src/components/Cell.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Cell(props) {
 
+    const [isClicked, setIsClicked] = useState(false);
+
     const handleClick = () => {
         console.log(`Cell #${props.num} clicked`);
-        console.log(`turn: ${props.turn}`);
-        props.onTurnChange();
-        
+        props.onCellClick(props.num);
+        if (!isClicked) {
+            setIsClicked(true);
+        }
+        else if (isClicked) {
+            // TODO: handle when cell is already filled/clicked
+            console.log('cell is already populated');
+        }
     }
 
     const cellClassName = () => {
@@ -14,15 +21,19 @@ function Cell(props) {
         if (props.num === 3 || props.num === 6 || props.num === 9) {
             text = 'last-cell'
         }
-        if (props.turn === "X")
-            return `cell cell-x ${text}`;
-        else 
-            return `cell cell-o ${text}`;
+        if (isClicked) {
+            if (props.value === "X")
+                return `cell cell-x ${text}`;
+            else 
+                return `cell cell-o ${text}`;
+        }
+        else {
+            return `cell ${text}`;
+        }
     }
 
     return (
         <div className={cellClassName()} onClick={handleClick}>
-
         </div>
     )
 }
--- webapp/tic-tac-toe/src/components/Main.js
+++ webapp/tic-tac-toe/src/components/Main.js
@@ -1,27 +1,16 @@
 import React, { useState } from 'react';
-import xImage from '../assets/X.png';
-import oImage from '../assets/O.png';
 import Cell from './Cell';
 
 function Main() {
 
-    // * Variables
-    const [turn, setTurn] = useState('X');
+    const [turn, setTurn] = useState("X");
     const [cells, setCells] = useState(Array(9).fill(""));
-    const [winner, setWinner] = useState();
-    const [isDraw, setIsDraw] = useState(false);
 
-    const handleClick = (num) => {
-        let arr = [...cells];
-        if (turn === 'X') {
-            arr[num] = "X";
-            setTurn("O"); 
-        }
-        else {
-            arr[num] = "O"
-            setTurn("O");
-        }
+    const handleCellClick = (num) => {
+        const arr = [...cells];
+        arr[num] = arr[num] ? null : turn;
         setCells(arr);
+        handleTurnChange();
     }
 
     const handleTurnChange = () => {
@@ -32,19 +21,19 @@ function Main() {
         <div className='body'>
             <div className='board'>
                 <div className='row'>
-                    <Cell num={1} turn={turn} onTurnChange={handleTurnChange} />
-                    <Cell num={2} turn={turn} onTurnChange={handleTurnChange} />
-                    <Cell num={3} turn={turn} onTurnChange={handleTurnChange} />
+                    <Cell num={1} value={cells[1]} onCellClick={handleCellClick} />
+                    <Cell num={2} value={cells[2]} onCellClick={handleCellClick} />
+                    <Cell num={3} value={cells[3]} onCellClick={handleCellClick} />
                 </div>
                 <div className='row'>
-                    <Cell num={4} turn={turn} onTurnChange={handleTurnChange} />
-                    <Cell num={5} turn={turn} onTurnChange={handleTurnChange} />
-                    <Cell num={6} turn={turn} onTurnChange={handleTurnChange} />
+                    <Cell num={4} value={cells[4]} onCellClick={handleCellClick} />
+                    <Cell num={5} value={cells[5]} onCellClick={handleCellClick} />
+                    <Cell num={6} value={cells[6]} onCellClick={handleCellClick} />
                 </div>
                 <div className='row last-row'>
-                    <Cell num={7} turn={turn} onTurnChange={handleTurnChange} />
-                    <Cell num={8} turn={turn} onTurnChange={handleTurnChange} />
-                    <Cell num={9} turn={turn} onTurnChange={handleTurnChange} />
+                    <Cell num={7} value={cells[7]} onCellClick={handleCellClick} />
+                    <Cell num={8} value={cells[8]} onCellClick={handleCellClick} />
+                    <Cell num={9} value={cells[9]} onCellClick={handleCellClick} />
                 </div>
             </div>
         </div>
